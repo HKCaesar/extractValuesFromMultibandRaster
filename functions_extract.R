0e11d680e5f52a9120699f20688f79366261031d
Lorena González Pérez <lorenagzp@gmail.com>
2020-05-20 10:20:50 -0700

    Added differenciated band naming depending on the imagery type
    This is custom processing adapted to specific file format, but can be adated to any other format

--- functions_extract.R
+++ functions_extract.R
@@ -29,6 +29,7 @@ askCSV <- function (default){
 #zones <- readOGR(dsn = fgdb, layer = roi_name) # This can read also GDB features
 askSHP <- function (default){
   f <- choose.files(default = "", multi = FALSE, caption = "Select the input  *.shp file", filters = matrix(c("Shapefile","*.shp"),1,2, byrow= TRUE))
+  print(paste("selected shp: ",f)) #Say which shp was selected
   return(shapefile(f)) # Return shapefile filename
 }
 
@@ -75,11 +76,12 @@ Q3_na <- function(x) quantile(x, prob=c(0.75), na.rm = TRUE)
 #######################################################################
 
 #### Extract vales from images
-extractThis <- function(r_file,zones, outFolder, ID_field ="Name",func=median_na,buf=0,small=FALSE,band_names=""){
+extractThis <- function(r_file,zones, outFolder, ID_field ="Name",func=median_na,buf=0,small=FALSE,band_names=NULL){
   ##   Imports raster as stack to allow multiple bands, uses "velox" object to make raster operations faster
   r <- velox(stack(r_file))
   #Set output name like the raster (remove extension and path from), save in the output folder
-  OutTableName <- file.path(outFolder,paste0(tools::file_path_sans_ext(basename(r_file)),".csv"),fsep=.Platform$file.sep)
+  outName <- tools::file_path_sans_ext(basename(r_file))
+  OutTableName <- file.path(outFolder,paste0(outName,".csv"),fsep=.Platform$file.sep)
   ## Zonal statistics with velox/raster package 
   #Calculate buffer if necessary, buffer = 0 means no buffer
   if (buf != 0 ) {
@@ -89,11 +91,56 @@ extractThis <- function(r_file,zones, outFolder, ID_field ="Name",func=median_na
   table <- r$extract(zones, df=TRUE, fun= func, small=small) #Extract and summarize the function, remove NA values. Returns a dataframe
   ## Set ID names from the shapefile IF field
   table$ID_sp <- zones@data[,ID_field] 
-  ##Set band names if they are given
-  if(band_names != "") {names(table) <- band_names}
+  ##Set band names if they are given and if they are not, assign them
+  i <- substring(outName,1,1) #Get the first letter of the raster name to know the camera used
+  # Assign band names depending on the camera used
+  band_names <- switch(i,
+                       "q" = c("ID","Red","Green","Red Edge","NIR"),
+                       "e" = c("ID","Blue","Red","Green","Red Edge","NIR"),
+                       "h" = c("ID",paste0("B",1:62)),
+                       "t" = c("ID","celsius"))
+  if(!is.null(band_names)) names(table) <- band_names #Assign the band names if we have them
   #Save to disk
   write.csv(table,OutTableName, row.names = FALSE)
+  ##plot histograms. the column list to use will be calculated
+  hist_extr(table, outName,outFolder)
 }
 
+#######################################################################
+#### Functions to perform graphics
+#### Libraries used: R-base code
+#######################################################################
 
-
+## Function to plot the histogram of selected extracted bands
+## t : is the extracted output table from "extractThis" [data frame]
+## outName : is a name to identify the output
+## column_list  : is the band # list of which bands to use for the histograms [integer vector]
+## par : is to divide the grid of graphs per page, default 3x3
+##  Consider that the first column is always the ID
+hist_extr <- function(t, outName, outFolder, column_list=NULL, par1=3){
+  tryCatch({
+      pdf(file.path(outFolder,sprintf("Histogram_%s.pdf",outName),fsep=.Platform$file.sep)) ## Start PDF
+      par <- par(mfrow=c(par1, par1))
+      #Select the bands to use based on the raster if they are not given
+      if (is.null(column_list)) {
+        i <- substring(outName, 1, 1) #Get the first letter of the raster name to know the camera used
+        # Indicate which band # to use, 
+        column_list <- switch(i, 
+                              "q" = c(1,2,3,4), 
+                              "e" = c(1,2,3,4,5), 
+                              "h" = c(6,22,37,43,60), 
+                              "t" = c(1,1))
+      }
+      if (is.null(column_list)) {column_list <- 2:ncol(t)-1}
+      #Print the histogram for each selected band
+      for (c in column_list) {
+        # the first column of the table would be the ID, so we skip it
+        hist(t[,c+1], nclass=20, main=sprintf("Histogram band: %i",c),xlab="Band value")
+      }
+      dev.off() ## Stop PDF
+      print(paste("saved PDF in outFolder")) #Say that it was saved the PDF
+    }#,
+    ##In case of halting error:
+    #error = function(e){print(c("An error ocurred: ",e$message))}#,
+  )
+}
