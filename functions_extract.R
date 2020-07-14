5a03b3422ba75bc763868fe32312d889c568c8a0
Lorena González Pérez <lorenagzp@gmail.com>
2020-07-14 10:32:19 -0700

    Update functions_extract.R
    Added the automatic handling of the band names for different predefined image types based on the first letter of the raster name.. Added check of the Coordinate reference system before extracting from image based on the zones (becasue if they are not the same they dont extract properly.)

--- functions_extract.R
+++ functions_extract.R
@@ -12,9 +12,9 @@ require(velox) # For fast raster processing
 
 #### Function to choose a CSV file interactively, halts if the input files or folders dont exist.
 ##   accepts to set the default location to open in the search window.
-askCSV <- function (default){
+askCSV <- function (defaultP=""){
   #Interactive ask CSV file
-  f <- choose.files(default = "", multi = FALSE, caption = "Select the input  *.CSV file", filters = matrix(c("CSV File","*.csv"),1,2, byrow= TRUE))
+  f <- choose.files(default = defaultP, multi = FALSE, caption = "Select the input  *.CSV file", filters = matrix(c("CSV File","*.csv"),1,2, byrow= TRUE))
   #Open data table CSV
   inputList <- read.csv(f,stringsAsFactors=FALSE)
   #check valid filenames 
@@ -27,16 +27,16 @@ askCSV <- function (default){
 #### Function to choose a shapefile file interactively.
 ##   accepts to set the default location to open in the search window.
 #zones <- readOGR(dsn = fgdb, layer = roi_name) # This can read also GDB features
-askSHP <- function (default){
-  f <- choose.files(default = "", multi = FALSE, caption = "Select the input  *.shp file", filters = matrix(c("Shapefile","*.shp"),1,2, byrow= TRUE))
+askSHP <- function (defaultP = ""){
+  f <- choose.files(default = defaultP, multi = FALSE, caption = "Select the input  *.shp file", filters = matrix(c("Shapefile","*.shp"),1,2, byrow= TRUE))
   print(paste("selected shp: ",f)) #Say which shp was selected
   return(shapefile(f)) # Return shapefile filename
 }
 
 #### Function to choose a raster file interactively, Allow Tif, Bsq and All
 ##   accepts to set the default location to open in the search window.}
-askRaster <- function (default){
-  f <- choose.files(default = "", multi = FALSE, caption = "Select the input raster", filters = matrix(c("BSQ","*.bsq","All","*","Tiff","*.tif"),3,2, byrow= TRUE))
+askRaster <- function (defaultP = ""){
+  f <- choose.files(default = defaultP, multi = FALSE, caption = "Select the input raster", filters = matrix(c("BSQ","*.bsq","All","*","Tiff","*.tif"),3,2, byrow= TRUE))
   print(paste("selected raster: ",f)) #Print the selected raster name
   return(f) # Return raster name 
 }
@@ -52,7 +52,9 @@ invalidInputFiles <- function(inputList){
 }
 
 #######################################################################
+####
 #### Statistics Functions removing NA values
+####
 #######################################################################
 ## Median removing NA
 median_na <- function(x) median(x, na.rm = TRUE)
@@ -76,10 +78,11 @@ Q3_na <- function(x) quantile(x, prob=c(0.75), na.rm = TRUE)
 #require(raster) #is already imported in this file
 #######################################################################
 
-#### Extract vales from images
-extractThis <- function(r_file,zones, outFolder, ID_field ="Name",func=median_na,buf=0,small=FALSE,band_names=NULL){
+#### Extract vales from images (velox package)
+extractThis <- function(r_file,zones, outFolder, ID_field ="Name",func=median_na,buf=0,small=TRUE,band_names=NULL){
   ##   Imports raster as stack to allow multiple bands, uses "velox" object to make raster operations faster
-  r <- velox(stack(r_file))
+  b <- brick(r_file) #Brick raster object
+  r <- velox(b) #Velox raster object
   #Set output name like the raster (remove extension and path from), save in the output folder
   outName <- tools::file_path_sans_ext(basename(r_file))
   OutTableName <- file.path(outFolder,paste0(outName,".csv"),fsep=.Platform$file.sep)
@@ -88,19 +91,28 @@ extractThis <- function(r_file,zones, outFolder, ID_field ="Name",func=median_na
   if (buf != 0 ) {
     zones <- buffer(zones, width = buf, dissolve= FALSE)
   }
+  #Check if they have the same Coordinate system, unify if necessary
+  if(proj4string(b) != proj4string(zones)){
+    message("Raster and zones dont have the same CRS, zones CRS will be transformed to the raster one (not permanently)")
+    zones <- spTransform(zones,CRS(proj4string(b)))
+  }
   ## With this we get a table with the values for all the plots and bands
   table <- r$extract(zones, df=TRUE, fun= func, small=small) #Extract and summarize the function, remove NA values. Returns a dataframe
-  ## Set ID names from the shapefile IF field
+  ## Set ID names from the shapefile ID field
   table$ID_sp <- zones@data[,ID_field] 
   ##Set band names if they are given and if they are not, assign them
-  i <- substring(outName,1,1) #Get the first letter of the raster name to know the camera used
+  ic <- substring(outName,1,1) #Get the first letter of the raster name to know the camera used
   # Assign band names depending on the camera used
-  band_names <- switch(i,
-                       "q" = c("ID","Red","Green","Red Edge","NIR"),
-                       "e" = c("ID","Blue","Red","Green","Red Edge","NIR"),
+  if(!is.null(band_names)){ #If the band names were passed (not NULL), use them
+    names(table) <- band_names
+  } else{#Or try to assign them based on the camera used (THis may not be valid for imagery subproducts)
+    band_names <- switch(ic,
+                       "q" = c("ID","Green","Red","Red Edge","NIR"),
+                       "e" = c("ID","Blue","Green","Red","Red Edge","NIR"),
                        "h" = c("ID",paste0("B",1:62)),
                        "t" = c("ID","celsius"))
-  if(!is.null(band_names)) names(table) <- band_names #Assign the band names if we have them
+    if(!is.null(band_names)) names(table) <- band_names #Assign the band names if we have a matching pattern
+  }
   #Save to disk
   write.csv(table,OutTableName, row.names = FALSE)
   ##plot histograms. the column list to use will be calculated
@@ -108,7 +120,7 @@ extractThis <- function(r_file,zones, outFolder, ID_field ="Name",func=median_na
 }
 
 #######################################################################
-#### Functions to perform graphics
+#### Functions to perform graphics and checks
 #### Libraries used: R-base code
 #######################################################################
 
@@ -118,21 +130,16 @@ extractThis <- function(r_file,zones, outFolder, ID_field ="Name",func=median_na
 ## column_list  : is the band # list of which bands to use for the histograms [integer vector]
 ## par : is to divide the grid of graphs per page, default 3x3
 ##  Consider that the first column is always the ID
-hist_extr <- function(t, outName, outFolder, column_list=NULL, par1=3){
+hist_extr <- function(t, outName, outFolder, par1=3){
   tryCatch({
       pdf(file.path(outFolder,sprintf("Histogram_%s.pdf",outName),fsep=.Platform$file.sep)) ## Start PDF
       par <- par(mfrow=c(par1, par1))
-      #Select the bands to use based on the raster if they are not given
-      if (is.null(column_list)) {
-        i <- substring(outName, 1, 1) #Get the first letter of the raster name to know the camera used
-        # Indicate which band # to use, 
-        column_list <- switch(i, 
-                              "q" = c(1,2,3,4), 
-                              "e" = c(1,2,3,4,5), 
-                              "h" = c(6,22,37,43,60), 
-                              "t" = c(1,1))
+      #Select the bands to use based on the raster (to avoid too many plots for hyper)
+      if (!ncol(t) > 6) { #If no more than 6 bands in the table, use them 
+        column_list <- seq_len(ncol(t)-1) #plot one histogram per band/column. But not he first (ID field)
+      } else{ #But if more, make a short sequence skiping bands
+        column_list <-seq(1,ncol(t)-1,by=ncol(t)/10)
       }
-      if (is.null(column_list)) {column_list <- 2:ncol(t)-1}
       #Print the histogram for each selected band
       for (c in column_list) {
         # the first column of the table would be the ID, so we skip it
@@ -144,4 +151,19 @@ hist_extr <- function(t, outName, outFolder, column_list=NULL, par1=3){
     ##In case of halting error:
     #error = function(e){print(c("An error ocurred: ",e$message))}#,
   )
+}
+
+#### Function to do some statistics for the yield components tables
+## Ongoing
+QA_yield <- function(t){
+  boxplot(t)
+  
+}
+
+#### Function to do some statistics for the extracted bands tables
+QA_bands <- function(t){
+  boxplot(t[,c("yield12","biomass","spikesm2","grainsm2")])
+  boxplot(t[,c("w1000","maturity", "pb","pn","kb","count","height")])
+  
+  
 }
